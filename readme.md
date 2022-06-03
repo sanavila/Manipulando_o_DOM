b9cb88bdb30f553069d799f27b461c740ad1626f
sanavila <rute.leone@hotmail.com>
2022-06-03 15:05:03 -0300

    deploy

--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
-#Preview
+# Project Link 
+# [Click here](https://sanavila.github.io/JS_Css_Display-flex/)
+
 <p align="center"> 
     <img src="https://raw.githubusercontent.com/sanavila/JS_Css_Display-flex/main/img/projeto.png">
 </p>
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const dateFirtJs = () => {     
-    result.innerHTML = Date()
-    title.innerHTML = "Estou muito orgulhoso de você!"
-    buttonDate.style.display = "none" 
+    result.innerHTML = Date();
+    title.innerHTML = "Estou muito orgulhoso de você!";
+    buttonDate.style.display = "none";
 } 
